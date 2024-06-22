e04b7f367a17328eef617e94ac608422cb4b0613
alearanc <alexisarancibia97@gmail.com>
2024-06-21 22:20:04 -0300

    cree la interface UpdatePersonaParams

---
+++ interfaces/UpdatePersonaParams.interface.ts
@@ -0,0 +1,14 @@
+import Localidad from "../models/Localidad";
+import { TipoUsuario } from "../models/TipoUsuario.enum";
+
+export interface UpdatePersonaParams {
+    id_usuario: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    password: string;
+    tipo_usuario: TipoUsuario;
+    telefono: string;
+    domicilio: string;
+    localidad: Localidad
+}
